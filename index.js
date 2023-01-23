dfe0b5873f8660e968a31661f2e42b6babcaec9f
Rajeev Hansda <rajeevrhansda@gmail.com>
2023-01-24 02:21:11 +0530

    added start command

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const express = require("express");
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req,res)=>{
-    res.send('Server is Running')
-})
+app.get("/", (req, res) => {
+  res.send("Server is Running");
+});
 
 app.listen(PORT, (error) => {
-  if (!error)
-    console.log(
-      "App is listening on port " + PORT
-    );
+  if (!error) console.log("App is listening on port " + PORT);
   else console.log("Error occurred, server can't start", error);
 });
