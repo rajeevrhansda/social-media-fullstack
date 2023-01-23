6aa2e89d96717f22ebd6385316e7b96750f90eff
Rajeev Hansda <rajeevrhansda@gmail.com>
2023-01-24 01:52:19 +0530

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require('dotenv')
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req,res)=>{
+    res.send('Server is Running')
+})
+
+app.listen(PORT, (error) => {
+  if (!error)
+    console.log(
+      "App is listening on port " + PORT
+    );
+  else console.log("Error occurred, server can't start", error);
+});
